d792b3a4e106215c66ea02f7e8365f8ed35657b0
Stone Chen <chen.stonechen@gmail.com>
2024-04-12 12:32:27 -0400

    unroll 8x8
    unroll

--- libavcodec/x86/vvc/vvc_sad.asm
+++ libavcodec/x86/vvc/vvc_sad.asm
@@ -3,8 +3,15 @@
 
 SECTION .text
 
+%macro MIN_MAX_SAD 3 ; bpc, op, count
+    vpminuw           %1, %2, %3
+    vpmaxuw           %3, %2, %3
+    vpsubusw          %3, %3, %1
+%endmacro
+
+
 INIT_YMM avx2
-cglobal vvc_sad_8x8_16bpc, 6, 9, 13, src1, src2, dx, dy, block_w, block_h, off1, off2, sad
+cglobal vvc_sad_8x8_16bpc, 6, 8, 13, src1, src2, dx, dy, block_w, block_h, off1, off2
     ; calculate initial offsets...gross
     sub               dxq, 2
     sub               dyq, 2
@@ -26,8 +33,6 @@ cglobal vvc_sad_8x8_16bpc, 6, 9, 13, src1, src2, dx, dy, block_w, block_h, off1,
 
     lea             src1q, [src1q + off1q * 2]
     lea             src2q, [src2q + off2q * 2]
-    xor              eax, eax
-    
 .load_8pixels:
         movu              xm0, [src1q]
         movu              xm1, [src2q]
@@ -36,7 +41,38 @@ cglobal vvc_sad_8x8_16bpc, 6, 9, 13, src1, src2, dx, dy, block_w, block_h, off1,
         vpsubusw          xm1, xm1, xm2
 
         vpmovzxwd          m1, xm1
-        
+
+        ; MIN_MAX_SAD m2, m0, m1
+
+        movu              xm5, [src1q + 128 * 2 * 2]
+        movu              xm6, [src2q + 128 * 2 * 2]
+        vpminuw           xm7, xm5, xm6
+        vpmaxuw           xm6, xm5, xm6
+        vpsubusw          xm6, xm6, xm7
+
+        vpmovzxwd          m6, xm6
+        vpaddd             m1, m6
+
+        movu              xm8, [src1q + 128 * 4 * 2]
+        movu              xm9, [src2q + 128 * 4 * 2]
+        vpminuw           xm10, xm8, xm9
+        vpmaxuw           xm9, xm8, xm9
+        vpsubusw          xm9, xm9, xm10
+
+        vpmovzxwd          m9, xm9
+        vpaddd             m1, m9
+
+        movu              xm11, [src1q + 128 * 6 * 2]
+        movu              xm12, [src2q + 128 * 6 * 2]
+        vpminuw           xm13, xm11, xm12
+        vpmaxuw           xm12, xm11, xm12
+        vpsubusw          xm12, xm12, xm13
+
+        vpmovzxwd          m12, xm12
+
+        vpaddd              m1, m12
+
+
         ; so this is the final step
         vextracti128      xm0, m1 , q0001 ;        3        2      1          0
         vpaddd            xm0, xm1        ; xm0 (7 + 3) (6 + 2) (5 + 1)   (4 + 0)
@@ -45,14 +81,15 @@ cglobal vvc_sad_8x8_16bpc, 6, 9, 13, src1, src2, dx, dy, block_w, block_h, off1,
         vpshufd           xm1, xm0, q0001 ; xm1    _      _     (5 1 7 3) (5 1 7 3)
         vpaddd            xm0, xm0, xm1   ;                               (01234567)
 
-        movd             sadd, xm0
-        add               eax, sadd
+        movd              eax, xm0
+        ;movd             sadd, xm0
+        ;add               eax, sadd
 
-        add             src1q, 4 * 128
-        add             src2q, 4 * 128
+        ;add             src1q, 2 * 128 * 2 
+        ;add             src2q, 2 * 128 * 2
 
-        sub          block_hd, 2
-        jg  .load_8pixel
+        ;sub          block_hd, 6
+        ;jg  .load_8pixels
 
     
 
