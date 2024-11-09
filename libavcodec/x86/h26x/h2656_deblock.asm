a08174927d7d854c60896e59d03c3d15aa472887
Stone Chen <chen.stonechen@gmail.com>
2024-11-09 09:41:21 -0500

    h2656_deblock.asm: add H2656_CHROMA_DEBLOCK
    Add H2656_CHROMA_DEBLOCK macro which shares some code between the vvc and hevc implementations for weak chroma / chroma deblock (respectively).

--- libavcodec/x86/h26x/h2656_deblock.asm
+++ libavcodec/x86/h26x/h2656_deblock.asm
@@ -670,3 +670,42 @@
     psubw            m8, m4, m12 ; q0 - delta0
     MASKED_COPY      m4, m8
 %endmacro
+
+%macro H2656_CHROMA_DEBLOCK 13
+    psubw            %5, %4, %3; q0 - p0
+    psubw            %8, %7, %6; p1 - q1
+    psllw            %5, 2; << 2
+    paddw            %8, %5;
+
+%ifidn %2, hevc
+    ;tc calculations
+    movq             m6, [tcq]; tc0
+    punpcklwd        m6, m6
+    pshufd           m6, m6, 0xA0; tc0, tc1
+%if cpuflag(ssse3)
+    psignw           m4, m6, [pw_m1]; -tc0, -tc1
+%else
+    pmullw           m4, m6, [pw_m1]; -tc0, -tc1
+%endif
+    ;end tc calculations
+%if %1 > 8
+    psllw            m4, %1-8; << (BIT_DEPTH - 8)
+    psllw            m6, %1-8; << (BIT_DEPTH - 8)
+%endif
+%endif
+
+    paddw            %8, [pw_4]; +4
+    psraw            %8, 3; >> 3
+
+%ifidn %2, hevc
+    pmaxsw           %8, %5
+    pminsw           %8, %9
+    paddw            %3, %8; p0 + delta0
+    psubw            %4, %8; q0 - delta0
+%elifidn %2, vvc
+    pmaxsw           %8, %12
+    pminsw           %8, %13
+    paddw           %10,  %3, %8
+    psubw           %11,  %4, %8
+%endif
+%endmacro
